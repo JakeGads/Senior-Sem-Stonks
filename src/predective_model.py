e6087279a7f015b88052764b686eda09453cc1d5
Jake Gadaleta <jake.gads@gmail.com>
2021-02-01 22:58:36 -0500

    included new packages

--- src/predective_model.py
+++ src/predective_model.py
@@ -1,15 +1,27 @@
+from os import write
 import pandas as pd
 from pandas_datareader import data as pdr
 import fix_yahoo_finance as yf 
 import datetime
+import matplotlib.pyplot as plt
+from matplotlib.pylab import rcParams
+from keras.models import Sequential
+from keras.layers import LSTM,Dropout,Dense
+from sklearn.preprocessing import MinMaxScaler
 
-def generate_stock_data(tag, start_date = pd.to_datetime('2017-01-01'), end_date = datetime.datetime.today()):
-    tag = [tag]
+rcParams['figure.figsize']=20,10
+
+def get_stock_data(tag, start_date, end_date):
+    if type(tag) != type([]):
+        tag = [tag]
     try:
-        data = pdr.get_data_yahoo(tag, start=start_date, end=end_date)
+        return pdr.get_data_yahoo(tag, start=start_date, end=end_date)
     except :
-        return 0
+        return pd.DataFrame.empty()
 
-
+def get_predictive_model(tag, start_date = pd.to_datetime('2017-01-01'), end_date = datetime.datetime.today()):
+    data = get_stock_data([tag], start_date, end_date)
+    print(data)
+    
 if __name__ == '__main__':
-    generate_stock_data('gamer')
+    get_predictive_model('gme')
