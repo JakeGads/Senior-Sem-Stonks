a5958383f81d727abd7f63c5505721785844c218
Jake Gadaleta <jake.gads@gmail.com>
2021-02-03 22:35:09 -0500

    added s tatic typing

--- src/predective_model.py
+++ src/predective_model.py
@@ -2,15 +2,8 @@ from os import write
 import pandas as pd
 from pandas_datareader import data as pdr
 import datetime
-import matplotlib.pyplot as plt
-from matplotlib.pylab import rcParams
-from keras.models import Sequential
-from keras.layers import LSTM,Dropout,Dense
-from sklearn.preprocessing import MinMaxScaler
 
-rcParams['figure.figsize']=20,10
-
-def get_stock_data(tag, start_date, end_date):
+def get_stock_data(tag: str, start_date: datetime.datetime, end_date: datetime.datetime) -> pd.core.frame.DataFrame:
     if type(tag) != type([]):
         tag = [tag]
     try:
@@ -19,8 +12,8 @@ def get_stock_data(tag, start_date, end_date):
         return pd.DataFrame.empty()
 
 def get_predictive_model(tag, start_date = pd.to_datetime('2017-01-01'), end_date = datetime.datetime.today()):
-    data = get_stock_data([tag], start_date, end_date)
-    print(data)
+    data = get_stock_data(tag, start_date, end_date)
+    print(type(data))
     
 if __name__ == '__main__':
     get_predictive_model('gme')
