1d459b9d95281dd4e675d61d97eafef677501c86
Jake Gadaleta <jake.gads@gmail.com>
2021-04-12 15:56:51 -0400

    syncing

--- src/predective_model.py
+++ src/predective_model.py
@@ -7,7 +7,10 @@ import pandas as pd
 import datetime as dt
 import numpy as np
 import tensorflow as tf # This code has been tested with TensorFlow 1.6
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import classification_report,confusion_matrix
 import os
 from platform import system as operating_system
 
@@ -18,13 +21,13 @@ def clear_static():
             #append the file name to the list
             print(os.path.join(root,file))
             os.remove(os.path.join(root,file))
-    
+
     for root, dirs, files in os.walk('static'):
         for file in files:
             #append the file name to the list
             print(os.path.join(root,file))
             os.remove(os.path.join(root,file))
-    
+
 def get_stock_data(tag: str, start_date: dt.datetime, end_date: dt.datetime) -> pd.core.frame.DataFrame:
     # forces tag into a list
     tag = [tag]
@@ -35,115 +38,57 @@ def get_stock_data(tag: str, start_date: dt.datetime, end_date: dt.datetime) -> 
         # reset the index to remove extra symbols
         data.reset_index(inplace=True,drop=False)
         # create a shorter data string
-        data['Date_String'] = data['Date'].dt.strftime('%Y-%m-%d') 
+        data['Date_String'] = data['Date'].dt.strftime('%Y-%m-%d')
         data = data.drop(["Adj Close", "Volume"], axis = 1)
 
 
         # returns the data
         return data
     except :
-        # if the data is wrong, return a blank one 
+        # if the data is wrong, return a blank one
         print("errord")
         return pd.DataFrame()
 
 def get_predictive_model(tag:str, start_date = pd.to_datetime('2020-01-01'), end_date = dt.datetime.today()):
     df = get_stock_data(tag, start_date, end_date)
-    
-    high_prices = df.loc[:,'High'].values
-    low_prices = df.loc[:,'Low'].values
-    mid_prices = (high_prices+low_prices)/2.0
-    
-    # 11_000
-    size = int(len(mid_prices) * (3/4))
-    train_data = mid_prices[:size]
-    test_data = mid_prices[size:]
-    
-    # Scale the data to be between 0 and 1
-    scaler = MinMaxScaler()
-    train_data = train_data.reshape(-1,1)
-    test_data = test_data.reshape(-1,1)
 
-    
-    smoothing_window_size = len(mid_prices) - size
-    
-    for di in range(0, int(size * .9),smoothing_window_size):
-        scaler.fit(train_data[di:di+smoothing_window_size,:])
-        train_data[di:di+smoothing_window_size,:] = scaler.transform(train_data[di:di+smoothing_window_size,:])
+    X = df['Close']
+    # y = df['High']
+    y = df['Date'].apply(lambda x:x.toordinal())
 
-    # You normalize the last bit of remaining data
-    try:
-        scaler.fit(train_data[smoothing_window_size:,:])
-    except :
-        print('All data already fixed')
 
-    try:
-        train_data[di+smoothing_window_size:,:] = scaler.transform(train_data[di+smoothing_window_size:,:])
-    except :
-        None
 
-    # Reshape both train and test data
-    train_data = train_data.reshape(-1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-    # Normalize test data
-    test_data = scaler.transform(test_data).reshape(-1)
+    scaler = StandardScaler()
 
-    # Exponential Smoothing
-    EMA = 0.0
-    gamma = 0.1
-    
-    for ti in range(size):
-        EMA = gamma*train_data[ti] + (1-gamma)*EMA
-        train_data[ti] = EMA
+    scaler.fit(X_train)
+    StandardScaler(copy=True, with_mean=True, with_std=True)
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
 
-    # saving here to make visualizations easier later
-    all_mid_data = np.concatenate([train_data,test_data],axis=0)
+    mlp = MLPClassifier(hidden_layer_sizes=(13,13,13),max_iter=500)
+    mlp.fit(X_train,y_train)
+    predictions = mlp.predict(X_test)
+    mlp.predict()
 
-    # exponential moving average
-    # very accurate, much wow
+    # plt.figure(figsize = (14,7))
+    # plt.plot(range(df.shape[0]),all_mid_data,color='b',label='True')
+    # plt.plot(range(0,N),run_avg_predictions,color='orange', label='Prediction')
+    # plt.xticks(range(0,df.shape[0],50),df['Date_String'].loc[::50],rotation=45)
+    # plt.xlabel('Date')
+    # plt.ylabel('Mid Price')
+    # plt.legend(fontsize=12)
 
-    window_size = int(size * .1)
-    N = train_data.size
+    # save_loc = f'..\\img\\{tag}' if operating_system() == 'Windows' else f'../img/{tag}'
 
-    run_avg_predictions = []
-    run_avg_x = []
-
-    mse_errors = []
-
-    running_mean = 0.0
-    run_avg_predictions.append(running_mean)
-
-    decay = 0.5
-
-    for pred_idx in range(1,N):
-        if pred_idx >= N:
-            date = dt.datetime.strptime(pred_idx, '%Y-%m-%d').date() + dt.timedelta(days=1)
-        else:
-            date = df.loc[pred_idx,'Date']
-
-        running_mean = running_mean*decay + (1.0-decay)*train_data[pred_idx-1]
-        run_avg_predictions.append(running_mean)
-        mse_errors.append((run_avg_predictions[-1]-train_data[pred_idx])**2)
-        run_avg_x.append(date)
-
-    print('MSE error for EMA averaging: %.5f'%(0.5*np.mean(mse_errors)))
-
-    plt.figure(figsize = (14,7))
-    plt.plot(range(df.shape[0]),all_mid_data,color='b',label='True')
-    plt.plot(range(0,N),run_avg_predictions,color='orange', label='Prediction')
-    plt.xticks(range(0,df.shape[0],50),df['Date_String'].loc[::50],rotation=45)
-    plt.xlabel('Date')
-    plt.ylabel('Mid Price')
-    plt.legend(fontsize=12)
-    
-    save_loc = f'..\\img\\{tag}' if operating_system() == 'Windows' else f'../img/{tag}'
-
-    try:
-        plt.savefig(save_loc)
-        plt.savefig(f'static\\{tag}' if operating_system() == 'Windows' else f'static/{tag}')
-    except :
-        save_loc = f'img\\{tag}' if operating_system() == 'Windows' else f'img/{tag}'
-        plt.savefig(save_loc)
-        plt.savefig(f'src\\static\\{tag}' if operating_system() == 'Windows' else f'src/static/{tag}')
+    # try:
+    #     plt.savefig(save_loc)
+    #     plt.savefig(f'static\\{tag}' if operating_system() == 'Windows' else f'static/{tag}')
+    # except :
+    #     save_loc = f'img\\{tag}' if operating_system() == 'Windows' else f'img/{tag}'
+    #     plt.savefig(save_loc)
+    #     plt.savefig(f'src\\static\\{tag}' if operating_system() == 'Windows' else f'src/static/{tag}')
 
 if __name__ == '__main__':
-    get_predictive_model('gme')
+    get_predictive_model('gme', start_date=pd.to_datetime('2020-01-01'))
