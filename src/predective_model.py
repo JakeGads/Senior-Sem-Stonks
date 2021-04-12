c6a6f19c373dee454b4f2c253788fd504ede34c2
Jake Gadaleta <jake.gads@gmail.com>
2021-04-12 18:27:26 -0400

    new imp

--- src/predective_model.py
+++ src/predective_model.py
@@ -1,19 +1,15 @@
 from pandas_datareader import data as pdr
 
-from sklearn.preprocessing import MinMaxScaler
-
-import matplotlib.pyplot as plt
-import pandas as pd
-import datetime as dt
 import numpy as np
-import tensorflow as tf # This code has been tested with TensorFlow 1.6
-from sklearn.preprocessing import StandardScaler
+import pandas as pd
+from matplotlib import pyplot as plt
+plt.style.use('dark_background')
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
 from sklearn.model_selection import train_test_split
-from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import classification_report,confusion_matrix
-import os
-from platform import system as operating_system
-
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Embedding, LSTM, GlobalMaxPooling1D, SpatialDropout1D
 
 def clear_static():
     for root, dirs, files in os.walk('src/static'):
@@ -52,25 +48,8 @@ def get_stock_data(tag: str, start_date: dt.datetime, end_date: dt.datetime) -> 
 def get_predictive_model(tag:str, start_date = pd.to_datetime('2020-01-01'), end_date = dt.datetime.today()):
     df = get_stock_data(tag, start_date, end_date)
 
-    X = df['Close']
-    # y = df['High']
-    y = df['Date'].apply(lambda x:x.toordinal())
-
-
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y)
-
-    scaler = StandardScaler()
-
-    scaler.fit(X_train)
-    StandardScaler(copy=True, with_mean=True, with_std=True)
-    X_train = scaler.transform(X_train)
-    X_test = scaler.transform(X_test)
-
-    mlp = MLPClassifier(hidden_layer_sizes=(13,13,13),max_iter=500)
-    mlp.fit(X_train,y_train)
-    predictions = mlp.predict(X_test)
-    mlp.predict()
+    df_train = df[len(df)*.75:]
+    df_test = df[:len(df)*.24]
 
     # plt.figure(figsize = (14,7))
     # plt.plot(range(df.shape[0]),all_mid_data,color='b',label='True')
@@ -91,4 +70,4 @@ def get_predictive_model(tag:str, start_date = pd.to_datetime('2020-01-01'), end
     #     plt.savefig(f'src\\static\\{tag}' if operating_system() == 'Windows' else f'src/static/{tag}')
 
 if __name__ == '__main__':
-    get_predictive_model('gme', start_date=pd.to_datetime('2020-01-01'))
+    get_predictive_model('gme')
