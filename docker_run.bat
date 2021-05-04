9b383a59a51ba0f3ed1e83ece9c30f5555d6c78a
Jake Gadaleta <jake.gads@gmail.com>
2021-05-03 23:40:41 -0400

    syncing

--- docker_run.bat
+++ docker_run.bat
@@ -1,4 +1,5 @@
+docker system prune -f
 docker volume rm vol
 docker rm -f stonks
-docker build -t stonks .
+docker build --no-cache -t stonks .
 docker run --name stonks -p 5001:5001 --mount source=vol,target=/app stonks
